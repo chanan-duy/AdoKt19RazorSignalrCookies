3be7ce88430cb2c836206121d3b0498b8f99dba6
chanan-duy <chanan.duy@proton.me>
2026-03-25 17:44:39 +0500

    Init

---
+++ AdoKt19RazorSignalrCookies/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdoKt19RazorSignalrCookies.Pages;
+
+public class IndexModel : PageModel
+{
+	public void OnGet()
+	{
+	}
+}
---
+++ AdoKt19RazorSignalrCookies/Pages/Privacy.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdoKt19RazorSignalrCookies.Pages;
+
+public class PrivacyModel : PageModel
+{
+	public void OnGet()
+	{
+	}
+}
